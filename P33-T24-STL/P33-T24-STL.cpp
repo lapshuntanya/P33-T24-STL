3edb4c1d13da40c68fbddff78c86d279e78ba860
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-04 17:23:36 +0300

    Add project files.

---
+++ P33-T24-STL/P33-T24-STL.cpp
@@ -0,0 +1,16 @@
+#include "MyArray.h"
+#include <string>
+
+int main()
+{
+    Array<int> a; // []
+    Array<int> b(5, 0); //[0, 0, 0, 0, 0]
+    Array<double> c(4, 9.99); //[9.99, 9.99, 9.99, 9.99]
+
+    a.print();
+    b.print();
+    c.print();
+
+
+}
+
